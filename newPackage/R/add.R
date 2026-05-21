a2895ddc8b2b6fdd8a5853439d5c605aaeae3e3b
lxin-sam <2904352428@qq.com>
2026-05-21 19:24:30 +0800

    first commit

---
+++ newPackage/R/add.R
@@ -0,0 +1,14 @@
+#' Return the sum of two numbers
+#'
+#' @param a Number one
+#' @param b Number two
+#'
+#' @returns Sum of two numbers
+#' @export
+#'
+#' @examples
+#' library(newPackage)
+#' add(2 , 5)
+add <- function(a, b){
+  a + b
+}
---
+++ newPackage/R/minus.R
@@ -0,0 +1,14 @@
+#' Return the minus of two numbers
+#'
+#' @param a Number one
+#' @param b Number two
+#'
+#' @returns minus of two numbers
+#' @export
+#'
+#' @examples
+#' library(newPackage)
+#' minus(2 , 5)
+minus <- function(a , b){
+  a - b
+}
